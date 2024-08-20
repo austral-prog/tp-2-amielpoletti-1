ffbb541e616a8c2d6dc483ee08b1863ec8bcd69c
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-08-20 17:22:52 -0300

    Update ejercicio 2.py

--- tp2 - python amiel/ejercicio 2.py
+++ tp2 - python amiel/ejercicio 2.py
@@ -1,8 +1,9 @@
-x = "bangladesh"
-y = "barbados"
+def earth():
+  x = "bangladesh"
+  y = "barbados"
 
-resultx= x < y
-resulty= x > y
+  resultx= x < y
+  resulty= x > y
 
-print(f"The result of {x} comes first in the dictionary than {y} is {resultx}")
-print(f"The result of {y} comes first in the dictionary than {x} is {resulty}")
+  print(f"The result of {x} comes first in the dictionary than {y} is {resultx}.")
+  print(f"The result of {y} comes first in the dictionary than {x} is {resulty}.")
