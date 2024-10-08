396306c72cde625db0bc174d503b3ac875fd198a
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-10-08 16:59:16 -0300

    Update change.py

--- change.py
+++ change.py
@@ -3,12 +3,16 @@ def change():
     dinero_recibido = 100
 
     vuelto = dinero_recibido - gasto
-
     pesos = int(vuelto)
     centavos = int((vuelto - pesos) * 100)
 
+    print("Ingresar gasto:")
+    print(f"{gasto}")
+    print("Dinero recibido")
+    print(f"{dinero_recibido}")
     print("\nVuelto\n")
     print("Pesos:")
     print(f"{pesos}")
     print("Centavos:")
     print(f"{centavos}")
+
